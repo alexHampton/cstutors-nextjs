25a429fe60383b1d71d2d82f9217ce2942f1e7c1
alexHampton <45002420+alexHampton@users.noreply.github.com>
2025-03-22 10:48:23 -0400

    use success and error messages

--- app/components/contact-form/contact-form.tsx
+++ app/components/contact-form/contact-form.tsx
@@ -70,6 +70,56 @@ export default function ContactForm({ contactMessage, setContactMessage }: Props
             </label>
             <button className="cs-button-solid cs-submit" type="submit">Send Message</button>
             <label style={{'display': 'none'}} htmlFor="bot-field">Bot-name: <input name="bot-field" type="text" /></label>
+
+
+            {status === 'ok' && (
+                <div className="alert alert-success">
+                    <SuccessIcon />
+                    Submitted!
+                </div>
+            )}
+            {status === 'error' && (
+                <div className="alert alert-error">
+                    <ErrorIcon />
+                    {error}
+                </div>
+            )}
         </form>
+    );
+}
+
+
+function SuccessIcon() {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="stroke-current shrink-0 h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+        >
+            <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+        </svg>
+    );
+}
+function ErrorIcon() {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="stroke-current shrink-0 h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+        >
+            <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+        </svg>
     );
 }
